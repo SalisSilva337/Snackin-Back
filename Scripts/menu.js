ab3218da11df7911cf04eeb5baabc207684a0e87
Salis Silva <salis.silva@aluno.senai.br>
2024-11-29 18:05:36 -0300

    hover na sidebar

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,86 +1,87 @@
 let modalSection = document.querySelector(".modalSection");
 let modalContent = document.querySelector(".modalContent");
+let closeButton = document.querySelector(".closeButton");
 let createItem = document.querySelector(".createItem");
 let categoryOptions = ["Lanche", "Acompanhamento", "Bebida", "Combo"];
 
 createItem.addEventListener("click", () => {
   modalSection.style.display = "flex";
-  modalContent.innerHTML = "";
+  // modalContent.innerHTML = "";
 
-  let labelDiv = document.createElement("div");
-  let createItemLabel = document.createElement("h3");
-  let closeButton = document.createElement("button");
+  // let labelDiv = document.createElement("div");
+  // let createItemLabel = document.createElement("h3");
+  // let closeButton = document.createElement("button");
 
-  let imgItemDivModal = document.createElement("div");
-  let imgItemModal = document.createElement("img");
+  // let imgItemDivModal = document.createElement("div");
+  // let imgItemModal = document.createElement("img");
 
-  //inputs
-  let inputsDiv = document.createElement("div");
+  // //inputs
+  // let inputsDiv = document.createElement("div");
 
-  let labelName = document.createElement("label");
-  labelName.textContent = "Nome";
-  let inputName = document.createElement("input");
+  // let labelName = document.createElement("label");
+  // labelName.textContent = "Nome";
+  // let inputName = document.createElement("input");
 
-  let labelCategory = document.createElement("label");
-  labelCategory.textContent = "Categoria";
-  let selectCategory = document.createElement("select");
+  // let labelCategory = document.createElement("label");
+  // labelCategory.textContent = "Categoria";
+  // let selectCategory = document.createElement("select");
 
-  let inputDescription = document.createElement("input");
-  inputDescription.className = "inputDescription";
+  // let inputDescription = document.createElement("input");
+  // inputDescription.className = "inputDescription";
 
-  let labelPrice = document.createElement("label");
-  labelPrice.textContent = "Preço";
-  let inputPrice = document.createElement("input");
+  // let labelPrice = document.createElement("label");
+  // labelPrice.textContent = "Preço";
+  // let inputPrice = document.createElement("input");
 
-  let mainItemDiv = document.createElement("div");
+  // let mainItemDiv = document.createElement("div");
 
-  //div de confirmacao
-  let buttonCreate = document.createElement("button");
-  let buttonCancel = document.createElement("button");
-  let confirmDiv = document.createElement("div");
+  // //div de confirmacao
+  // let buttonCreate = document.createElement("button");
+  // let buttonCancel = document.createElement("button");
+  // let confirmDiv = document.createElement("div");
 
-  createItemLabel.textContent = "Crie um novo Item";
-  closeButton.textContent = "X";
+  // createItemLabel.textContent = "Crie um novo Item";
+  // closeButton.textContent = "X";
 
-  buttonCancel.textContent = "Cancelar";
-  buttonCreate.textContent = "Criar";
+  // buttonCancel.textContent = "Cancelar";
+  // buttonCreate.textContent = "Criar";
 
-  labelDiv.className = "labelDiv";
-  labelDiv.appendChild(createItemLabel);
-  labelDiv.appendChild(closeButton);
+  // labelDiv.className = "labelDiv";
+  // labelDiv.appendChild(createItemLabel);
+  // labelDiv.appendChild(closeButton);
 
-  imgItemDivModal.className = "imgItemDivModal";
-  imgItemDivModal.appendChild(imgItemModal);
+  // imgItemDivModal.className = "imgItemDivModal";
+  // imgItemDivModal.appendChild(imgItemModal);
 
-  for (let index = 0; index < categoryOptions.length; index++) {
-    let option = document.createElement("option");
-    option.textContent = categoryOptions[index];
-    option.value = categoryOptions[index];
-    selectCategory.appendChild(option);
-  }
+  // for (let index = 0; index < categoryOptions.length; index++) {
+  //   let option = document.createElement("option");
+  //   option.textContent = categoryOptions[index];
+  //   option.value = categoryOptions[index];
+  //   selectCategory.appendChild(option);
+  // }
 
-  inputsDiv.className = "inputsDiv";
-  mainItemDiv.className = "mainItemDiv";
+  // inputsDiv.className = "inputsDiv";
+  // mainItemDiv.className = "mainItemDiv";
 
-  mainItemDiv.appendChild(imgItemDivModal);
-  inputsDiv.appendChild(labelName);
-  inputsDiv.appendChild(inputName);
-  inputsDiv.appendChild(labelCategory);
-  inputsDiv.appendChild(selectCategory);
+  // mainItemDiv.appendChild(imgItemDivModal);
+  // inputsDiv.appendChild(labelName);
+  // inputsDiv.appendChild(inputName);
+  // inputsDiv.appendChild(labelCategory);
+  // inputsDiv.appendChild(selectCategory);
 
-  inputsDiv.appendChild(inputDescription);
+  // inputsDiv.appendChild(inputDescription);
 
-  inputsDiv.appendChild(labelPrice);
-  inputsDiv.appendChild(inputPrice);
+  // inputsDiv.appendChild(labelPrice);
+  // inputsDiv.appendChild(inputPrice);
 
-  mainItemDiv.appendChild(inputsDiv);
+  // mainItemDiv.appendChild(inputsDiv);
 
-  confirmDiv.appendChild(buttonCancel);
-  confirmDiv.appendChild(buttonCreate);
+  // confirmDiv.appendChild(buttonCancel);
+  // confirmDiv.appendChild(buttonCreate);
 
-  modalContent.appendChild(labelDiv);
-  modalContent.appendChild(mainItemDiv);
-  modalContent.appendChild(confirmDiv);
+  // modalContent.appendChild(labelDiv);
+  // modalContent.appendChild(mainItemDiv);
+  // modalContent.appendChild(confirmDiv);
 
   closeButton.addEventListener("click", () => {
     modalSection.style.display = "none";
--- Scripts/stackinBack.js
+++ Scripts/stackinBack.js
@@ -2,6 +2,7 @@ let iframe = document.querySelector(".iframe");
 let homeButton = document.querySelector(".homeButton");
 let menuButton = document.querySelector(".menuButton");
 let ordersButton = document.querySelector(".ordersButton");
+let sideBar = document.querySelector(".sideBar");
 let settingsButton = document.querySelector(".settingsButton");
 
 //Change Iframe Source

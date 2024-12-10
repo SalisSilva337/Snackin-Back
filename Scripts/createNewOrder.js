e56e74e68a609bff278410b239f0947bb08791e0
salis <salis.silva@aluno.senai.br>
2024-12-09 21:30:56 -0300

    melhorias na pagina de criar novo pedido

---
+++ Scripts/createNewOrder.js
@@ -0,0 +1,74 @@
+let spanSelectedButton = document.querySelector("#spanSelectedButton");
+let buttonsPlaces = document.querySelectorAll(".buttonsLocal");
+
+buttonsPlaces.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.value);
+    spanSelectedButton.textContent = "Selecionado: " + btn.value;
+  });
+});
+
+//Client Data Part
+let inputsClientData = document.querySelectorAll(".inputsClientData");
+let selectPayment = document.querySelector(".selectPayment");
+
+inputsClientData.forEach((input) => {
+  input.addEventListener("change", () => {
+    if (input.value == "") {
+      input.classList.add("emptyInput");
+      input.classList.remove("nonEmptyInput");
+    } else if (input.value.trimStart() !== input.value) {
+      input.classList.add("emptyInput");
+      input.classList.remove("nonEmptyInput");
+    } else {
+      input.classList.remove("emptyInput");
+      input.classList.add("nonEmptyInput");
+    }
+  });
+});
+
+selectPayment.addEventListener("change", () => {
+  console.log(selectPayment.value);
+  if (selectPayment.value == "") {
+    selectPayment.classList.add("emptyInput");
+    selectPayment.classList.remove("nonEmptyInput");
+  } else {
+    selectPayment.classList.remove("emptyInput");
+    selectPayment.classList.add("nonEmptyInput");
+  }
+});
+
+//Address Part
+
+let cep = document.querySelector("#cepTxt");
+let inputsPlace = document.querySelectorAll(".inputsPlace");
+
+cep.addEventListener("change", () => {
+  cepvalue = cep.value;
+  if (cepvalue !== "") {
+    let url = "https://brasilapi.com.br/api/cep/v1/" + cepvalue;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+
+    request.onload = function () {
+      if (request.status === 200) {
+        let endereco = JSON.parse(request.response);
+        document.querySelector("#ruaTxt").value = endereco.street;
+        document.querySelector("#bairroTxt").value = endereco.neighborhood;
+        inputsPlace.forEach((input) => {
+          input.classList.remove("emptyInput");
+          input.classList.add("nonEmptyInput");
+        });
+      } else {
+        document.querySelector("#ruaTxt").value = "CEP INVÁLIDO";
+        document.querySelector("#bairroTxt").value = "CEP INVÁLIDO";
+        inputsPlace.forEach((input) => {
+          input.classList.add("emptyInput");
+          input.classList.remove("nonEmptyInput");
+        });
+      }
+    };
+  }
+});

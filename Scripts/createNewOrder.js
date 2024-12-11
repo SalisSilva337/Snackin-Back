9dfa41ffb4e207c0231c645ed2e9906fb6008f07
salis <salis.silva@aluno.senai.br>
2024-12-11 20:50:38 -0300

    95% concluido criar novo pedido

--- Scripts/createNewOrder.js
+++ Scripts/createNewOrder.js
@@ -73,28 +73,6 @@ cep.addEventListener("change", () => {
   }
 });
 
-//ItemCount func
-let itemCount = document.querySelector(".itemCount");
-let subButton = document.querySelector("#subButton");
-let sumButton = document.querySelector("#sumButton");
-
-let count = 1;
-
-itemCount.value = count;
-
-subButton.addEventListener("click", () => {
-  if (count <= 1) {
-    count = 1;
-  } else {
-    count--;
-    itemCount.value = count;
-  }
-});
-sumButton.addEventListener("click", () => {
-  count++;
-  itemCount.value = count;
-});
-
 //Block Divs func
 let gridLocal = document.querySelector("#gridLocal");
 let gridData = document.querySelector("#gridData");
@@ -188,3 +166,90 @@ backToData.addEventListener("click", () => {
 });
 
 finishOrder.addEventListener("click", () => {});
+
+// let itemImage = document.querySelector(".itemImage");
+// let productName = document.querySelector("#productName");
+// // let addToCart = document.querySelector(".addToCart");
+let ItemsTable = document.querySelector(".ItemsTable");
+
+const options = {
+  method: "GET",
+};
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=burger", options)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    for (let index = 0; index < data.meals.length; index++) {
+      let tableGrid = document.createElement("div");
+      tableGrid.className = "tableGrid";
+      let itemImage = document.createElement("div");
+      itemImage.className = "itemImage";
+      let productName = document.createElement("h2");
+      productName.id = "productName";
+      let productPrice = document.createElement("h3");
+      productPrice.id = "productPrice";
+      let productInfos = document.createElement("div");
+      productInfos.appendChild(productName);
+      productInfos.appendChild(productPrice);
+      let itemsCountDiv = document.createElement("div");
+      itemsCountDiv.className = "itemsCountDiv";
+      let subButton = document.createElement("button");
+      subButton.className = "itemsCountButtons";
+      let itemCount = document.createElement("input");
+      itemCount.className = "itemCount";
+      let sumButton = document.createElement("button");
+      sumButton.className = "itemsCountButtons";
+      let addToCart = document.createElement("button");
+      addToCart.className = "addToCart";
+
+      let productImg = document.createElement("img");
+
+      productImg.src = data.meals[index].strMealThumb;
+      productName.textContent = data.meals[index].strMeal;
+      productPrice.textContent = "R$" + (Math.random(1) * 50).toFixed(2);
+      let cutProductName = productName.textContent.slice(0, 10) + "...";
+      productName.textContent = cutProductName;
+      itemImage.appendChild(productImg);
+
+      let cartImg = document.createElement("img");
+      cartImg.src = "../../Imgs/icons/shoppingCartIcon.svg";
+      addToCart.appendChild(cartImg);
+
+      subButton.textContent = "-";
+      sumButton.textContent = "+";
+      itemCount.type = "number";
+      itemCount.disabled = true;
+
+      //ItemCount func
+
+      let count = 1;
+
+      itemCount.value = count;
+
+      subButton.addEventListener("click", () => {
+        if (count <= 1) {
+          count = 1;
+        } else {
+          count--;
+          itemCount.value = count;
+        }
+      });
+      sumButton.addEventListener("click", () => {
+        count++;
+        itemCount.value = count;
+      });
+      tableGrid.appendChild(itemImage);
+      tableGrid.appendChild(productInfos);
+
+      itemsCountDiv.appendChild(subButton);
+      itemsCountDiv.appendChild(itemCount);
+      itemsCountDiv.appendChild(sumButton);
+
+      tableGrid.appendChild(itemsCountDiv);
+
+      tableGrid.appendChild(addToCart);
+      ItemsTable.appendChild(tableGrid);
+    }
+  });
